e7ca62d831e6a4ff95e49419e2337f682b406b79
Candy Fair <candice.bastien@gmail.com>
2022-08-30 17:30:23 +0200

    Update create_tables.sql

--- docs/create_tables.sql
+++ docs/create_tables.sql
@@ -64,12 +64,12 @@ INSERT INTO "step_type"("code", "name") VALUES
 ('other', 'Other');
 
 INSERT INTO "step"("trip_id", "position", "travel_name", "start_date", "end_date", "start_time", "end_time", "point_departure", "point_arrival", "details", "step_type") VALUES
-(1, 1, 'Eurostar', '07/21/2022', '07/21/2022', '15:13:00', '16:30:00', 'Paris', 'London', 'Coach 10, 55/56', 1),
-(2, 1, 'Thalys', '08/15/2022', '08/15/2022', '09:55:00', '13:16:00', 'Paris', 'Cologne', 'Voiture 27', 1),
-(1, 2, 'Caledonian Sleeper', '07/21/2022','07/21/2022', '21:15:00', '08:47:00', 'London', 'Inverness', 'Coach M, 06U/06L', 2),
-(1, 3, 'Train', '07/22/2022','07/22/2022', '10:56:00', '13:35:00', 'Inverness', 'Kyle of Lochlash', '', 2),
-(1, 4, 'Car hire', '07/22/2022','07/22/2022', '14:00:00', '15:00:00', 'Kyle of Lochlash', 'Broadford', '', 3),
-(2, 2, 'Dorint an der Messe', '08/15/2022', '08/16/2022', '00:00:00', '00:00:00', 'Cologne', 'Cologne', 'Deutz-Müleimer-Str', 4);
+(1, 1, 'Eurostar', '07/21/2022', '07/21/2022', '15:13:00', '16:30:00', 'Paris', 'London', 'Coach 10, 55/56', 2),
+(2, 1, 'Thalys', '08/15/2022', '08/15/2022', '09:55:00', '13:16:00', 'Paris', 'Cologne', 'Voiture 27', 2),
+(1, 2, 'Caledonian Sleeper', '07/21/2022','07/21/2022', '21:15:00', '08:47:00', 'London', 'Inverness', 'Coach M, 06U/06L', 1),
+(1, 3, 'Train', '07/22/2022','07/22/2022', '10:56:00', '13:35:00', 'Inverness', 'Kyle of Lochlash', '', 1),
+(1, 4, 'Car hire', '07/22/2022','07/22/2022', '14:00:00', '15:00:00', 'Kyle of Lochlash', 'Broadford', '', 4),
+(2, 2, 'Dorint an der Messe', '08/15/2022', '08/16/2022', '00:00:00', '00:00:00', 'Cologne', 'Cologne', 'Deutz-Müleimer-Str', 9);
 
 
 COMMIT;
